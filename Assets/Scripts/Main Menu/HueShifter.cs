290d2d3415af1f6d6d783f279203248c33fae868
MrSliddes <MrSliddes@outlook.com>
2020-03-12 13:04:01 +0100

    Lesweek 6
    12-03-2020

---
+++ Assets/Scripts/Main Menu/HueShifter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Shift hue of an image
+/// </summary>
+public class HueShifter : MonoBehaviour
+{
+    public float Speed = 1;
+    private Image rend;
+
+    void Start()
+    {
+        rend = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        rend.material.SetColor("_Color", HSBColor.ToColor(new HSBColor(Mathf.PingPong(Time.time * Speed, 1), 1, 1)));
+    }
+}
+
---
+++ Assets/Scripts/Main Menu/ScrollTexture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollTexture : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vOffset = Time.deltaTime * scrollSpeed;
+        image.material.SetTextureOffset("_MainTex", new Vector2(vOffset, vOffset));
+    }
+}
